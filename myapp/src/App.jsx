f83bcbde630521836db473bf409c99434339c2bf
unknown <you@example.com>
2025-12-03 21:38:28 +0400

    test

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import {Router, Routes, Route} from 'react-router' 
+import {BrowserRouter as Routes, Route} from 'react-router' 
 import Navbar from './Components/Navbar'
 
 import Home from './Pages/Home/home'
 import About from './Pages/About/about'
-import Contact from './Pages/Contact/contact'
+import Contact from './Pages/Contact/contact.jsx'
 import House from './Pages/Houses/house'
 import HouseDetails from './Pages/Houses/housedetails'
 import SignUp from './auth/signup'
@@ -14,9 +14,11 @@ const App = () => {
   return (
     <div className='bg-blue-300'>
 
+      <Navbar/>
 
-
-
+        <Routes>
+          <Route path='/contact' element = {<Contact/>}/>
+        </Routes>
 
     </div>
   )
--- myapp/src/Pages/Contact/contact.jsx
+++ myapp/src/Pages/Contact/contact.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>contact</div>
+    <div className='bg-blue-500'>
+
+      <h1>Contact with US!!!</h1>
+
+      <div className='bg-white'>
+        <input type="text" placeholder='your name'/>
+        <input type="text" placeholder='your message'/>
+        <button>Send us</button>
+      </div>
+
+    </div>
   )
 }
 
