25ca830ff2b1119119eee2b2d4280e3ba65e24a2
LalaAslanova <lalAslanova830@gmail.com>
2025-12-03 23:33:37 +0400

    navbar

--- myapp/src/Components/Navbar.jsx
+++ myapp/src/Components/Navbar.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header className="fixed top-0 left-0 w-full py-5 px-[10%] bg-sky-400 flex justify-between items-center z-100">
+      <Link to="/" className="text-white text-3xl font-bold">MyHome</Link>
 
-export default Navbar
+      <nav className="flex">
+        <Link to="#home" className="text-white text-lg font-medium ml-10 border-b-2 border-transparent hover:border-white transition-all">home</Link>
+
+        <Link to="#about" className="text-white text-lg font-medium ml-10 border-b-2 border-transparent hover:border-white transition-all">about</Link>
+
+        <Link to="#services" className="text-white text-lg font-medium ml-10 border-b-2 border-transparent hover:border-white transition-all">services</Link>
+
+        <Link to="#contact" className="text-white text-lg font-medium ml-10 border-b-2 border-transparent hover:border-white transition-all">contact</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
