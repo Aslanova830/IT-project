a15a44191962aebab2f4ddaa5c4e315371683c2b
unknown <you@example.com>
2025-12-05 14:14:11 +0400

    stil

--- myapp/src/Components/Footer.jsx
+++ myapp/src/Components/Footer.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='bg-sky-400 w-full flex justify-between'>
+
+      <div className="flex flex-col ml-[40px] mt-[40px] mb-[20px]">
+        <Link className="text-xl text-white font-medium" to='/home'>Home</Link>
+        <br />
+        <Link className="text-xl text-white font-medium" to='/about'>About</Link>
+        <br />
+        <Link className="text-xl text-white font-medium" to='/house'>Services</Link>
+        <br />
+        <Link className="text-xl text-white font-medium" to='/contact'>Contacts</Link>
+      </div>  
+
+      <div className="mr-[150px] mt-[60px]">
+        <h1 className="text-2xl text-white font-medium">Contact with us:</h1>
+        <button className="text-xl bg-black text-white p-5 font-semibold rounded-2xl"><Link to='/contact'>Contact</Link></button>
+      </div>
+
+    </div>
   )
 }
 
