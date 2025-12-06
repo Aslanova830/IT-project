6bec54e7657526cc1c41a4b5177d53cb396271c9
ilham2032 <ilhamshabanov@mail.ru>
2025-12-06 12:32:23 +0400

    finish

--- myapp/src/Pages/About/about.jsx
+++ myapp/src/Pages/About/about.jsx
@@ -4,15 +4,59 @@ const About = () => {
   return (
     <div className="pt-[70px]">
       <h1 className="text-center font-bold text-7xl p-[40px]">About US</h1>
-      <p className="text-center text-3xl font-semibold p-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ipsa distinctio perferendis expedita sequi voluptate impedit asperiores id eius vel, accusantium maiores in ullam? Excepturi eveniet expedita officia porro corporis.</p>
-      <p className="text-center text-3xl font-semibold ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ipsa distinctio perferendis expedita sequi voluptate impedit asperiores id eius vel, accusantium maiores in ullam? Excepturi eveniet expedita officia porro corporis.</p>
-      <p className="font-bold text-center text-red-600 text-4xl p-[30px]">ORDER RIGHT NOW</p>
-      <button className="text-center"><Link to='/house'>ORDER</Link></button>
-    
-      <p>By IT INNOVATIONS</p> 
+      <p className="text-center text-3xl font-semibold p-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ipsa
+        distinctio perferendis expedita sequi voluptate impedit asperiores id
+        eius vel, accusantium maiores in ullam? Excepturi eveniet expedita
+        officia porro corporis.
+      </p>
+      <p className="text-center text-3xl font-semibold ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ipsa
+        distinctio perferendis expedita sequi voluptate impedit asperiores id
+        eius vel, accusantium maiores in ullam? Excepturi eveniet expedita
+        officia porro corporis.
+      </p>
+      <p className="font-bold text-center text-red-600 text-4xl p-[30px]">
+        ORDER RIGHT NOW
+      </p>
 
+      <div className="flex justify-center items-center pb-[30px]">
+        <button className="bg-blue-400 text-white font-medium rounded-xl p-4">
+          <Link to="/house">ORDER</Link>
+        </button>
+      </div>
+
+      <h1 className="text-center font-semibold text-5xl pt-[30px]">
+        Our Organizers:
+      </h1>
+
+      <div className="flex justify-center items-center">
+        <div className="m-[30px] border-[1px] border-gray-300 p-[20px] rounded-xl">
+          <i className="fa-solid fa-user text-3xl ml-[40px]"></i>
+          <h3 className="font-semibold text-3xl">Ilham Sh</h3>
+          <p className="font-medium text-xl ml-[20px]">Founder</p>
+        </div>
+
+        <div className="m-[30px] border-[1px] border-gray-300 p-[20px] rounded-xl">
+          <i className="fa-solid fa-user text-3xl ml-[30px]"></i>
+          <h3 className="font-semibold text-3xl">Lale A.</h3>
+          <p className="font-medium text-xl ml-[10px]">Founder</p>
+        </div>
+
+        <div className="m-[30px] border-[1px] border-gray-300 p-[20px] rounded-xl">
+          <i className="fa-solid fa-user text-3xl ml-[50px]"></i>
+          <h3 className="font-semibold text-3xl ml-[10px]">Ayxan A.</h3>
+          <p className="font-medium text-xl ml-[10px]">SMM worker</p>
+        </div>
+
+        <div className="m-[30px] border-[1px] border-gray-300 p-[20px] rounded-xl">
+          <i className="fa-solid fa-user text-3xl ml-[30px]"></i>
+          <h3 className="font-semibold text-3xl ml-[5px]">Indji A.</h3>
+          <p className="font-medium text-xl ml-[10px] ">Assistant</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default About
--- myapp/src/Pages/Contact/contact.jsx
+++ myapp/src/Pages/Contact/contact.jsx
@@ -1,18 +1,55 @@
 import React from 'react'
 
 const Contact = () => {
+
+  const OnClick =() => {
+    window.alert('The message was sent. Thank you!')
+  }
+
+
   return (
-    <div className='p-[70px] flex items-center justify-center'>
-      
-      <form className=' border-gray-200 rounded-2xl m-[200px]'>
-        <input className='border-[1px] rounded-2xl border-gray-400 p-6 mb-[20px]' type="text" placeholder='name'/>
+    <div className="p-[70px] flex items-center justify-between">
+      <div className='ml-[50px] mt-[50px]'>
+        <div>
+          <h1 className='font-semibold text-2xl '>Contact with us:</h1>
+          <p className='font-medium text-xl'>+994 55 718 68 58</p>
+        </div>
+
         <br />
-        <input className='border-[1px] rounded-2xl border-gray-400 p-6' type="text" placeholder='reason'/>
-        <br />
-        <button className='bg-blue-400 text-white text-[23px] p-2 rounded-[10px] mt-[40px]'>Send a message</button>
-      </form>
+        <div>
+          <h1 className='font-semibold text-2xl'>Place:</h1>
+          <p className='font-medium text-xl'>IT INNOVATIONS ACADEMY</p>
+        </div>
+
+<br />
+        <div>
+          <h1 className='font-semibold text-2xl'>Email us:</h1>
+          <p className='font-medium text-xl'>ilhamshabanov965@gmail.com</p>
+        </div>
+      </div>
+
+      <div className='border-[3px] border-gray-200 rounded-xl mt-[40px] mr-[40px]'>
+        <form className=" border-gray-200 rounded-2xl p-5">
+          <input
+            className="border-[1px] rounded-2xl border-gray-400 p-6 mb-[20px]"
+            type="text"
+            placeholder="name"
+          />
+          <br />
+          <input
+            className="border-[1px] rounded-2xl border-gray-400 p-6"
+            type="text"
+            placeholder="write your message here"
+          />
+          <br />
+          <button onClick={OnClick} className="bg-blue-400 text-white text-[23px] p-2 rounded-[10px] mt-[40px]" >
+            Send a message 
+          </button>
+        </form>
+      </div>
+
     </div>
-  )
+  );
 }
 
 export default Contact;
